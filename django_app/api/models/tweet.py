422bfeccea5b877086f2b8ffd0bad98483f0ac24
xKxAxKx <pekepooooong@gmail.com>
2017-09-08 17:09:52 +0900

    [fix]tweetモデル内のuser参照を変更

--- django_app/api/models/tweet.py
+++ django_app/api/models/tweet.py
@@ -1,9 +1,9 @@
 from django.db import models
-from api.models.user import Account
+from django.conf import settings
 
 class Tweet(models.Model):
     tweet = models.CharField(max_length=140, blank=False)
-    user = models.ForeignKey(Account, verbose_name='ユーザー', related_name='posts_editor')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='ユーザー', related_name='posts_editor')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
