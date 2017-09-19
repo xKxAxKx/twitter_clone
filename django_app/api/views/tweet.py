aa31b10b050c7ec84cfd093b59fe52b0e5b82ef1
xKxAxKx <pekepooooong@gmail.com>
2017-09-19 21:23:25 +0900

    戻し

--- django_app/api/views/tweet.py
+++ django_app/api/views/tweet.py
@@ -10,17 +10,18 @@ from rest_framework import status, viewsets, filters
 from rest_framework.views import APIView
 from api.serializers.tweet import TweetSerializer
 from api.models.tweet import Tweet
+from api.models.user import Account
 
 
 # ツイート作成のView(POST)
 class TweetPostView(generics.CreateAPIView):
-    permission_classes = (permissions.AllowAny,) #あとでpermissions.IsAuthenticatedに戻す
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = TweetSerializer
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
-        request.data['user'] = request.user
-        return super().create(request, *args, **kwargs)
+        request.data['user'] = self.request.user
+        return super().create(request)
 
 
 class TweetListPagination(PageNumberPagination):
