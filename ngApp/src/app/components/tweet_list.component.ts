1a0be07d9d0f8041a61308c0ca9f7daf034019e9
xKxAxKx <pekepooooong@gmail.com>
2017-10-01 21:11:38 +0900

    update

--- ngApp/src/app/components/tweet_list.component.ts
+++ ngApp/src/app/components/tweet_list.component.ts
@@ -8,14 +8,16 @@ import { TweetStore } from '../stores/tweet.store';
 @Component({
   selector: 'tweet-list',
   template: `
-    <div *ngFor="let tweet of tweetStore.tweetlist">
-      <div class="panel panel-primary">
-        <div class="panel-body">
-          <p>
-            <a [routerLink]="['/user', tweet.user.id]">@{{ tweet.user.username }}</a>
-            <span>{{ tweet.created_at | date: 'yyyy/MM/dd hh:mm:ss' }}</span>
-          </p>
-          {{ tweet.tweet }}
+    <div *ngIf="tweetStore.tweetlist">
+      <div *ngFor="let tweet of tweetStore.tweetlist">
+        <div class="panel panel-primary">
+          <div class="panel-body">
+            <p>
+              <a [routerLink]="['/user', tweet.user.id]">@{{ tweet.user.username }}</a>
+              <span>{{ tweet.created_at | date: 'yyyy/MM/dd hh:mm:ss' }}</span>
+            </p>
+            {{ tweet.tweet }}
+          </div>
         </div>
       </div>
     </div>
@@ -32,7 +34,6 @@ export class TweetListComponent {
   users = null
 
   ngOnInit() {
-    this.tweetStore.tweetlist = {};
     // URLのパラメータのidを取得する
     this.activatedRoute.params.subscribe((params: Params) => {
         this.users = params['user_id'];
