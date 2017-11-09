29e6c0bc70e55588b7bb8543e89c11098d824213
kakutani <pekepooooong@gmail.com>
2017-11-09 19:34:06 +0900

    [update]flash messageの対応

--- ngApp/src/app/stores/common.store.ts
+++ ngApp/src/app/stores/common.store.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { TweetService } from '../services/tweet.service';
 
@@ -17,9 +17,14 @@ export class CommonStore {
     private router: Router,
   ) {
 
-    router.events.subscribe(() => {
-      this.errorMessage = '';
-      this.successMessage = '';
+    router.events
+      .filter(event => event instanceof NavigationStart)
+      .subscribe(() => {
+        this.counter -= 1;
+        if(this.counter <= 0) {
+          this.errorMessage = '';
+          this.successMessage = '';
+        }
     });
 
     // ログインに成功したメッセージを表示する
@@ -27,18 +32,21 @@ export class CommonStore {
     this.userService.successUserLoginSubject.subscribe( () =>{
       this.successMessage = 'Success Login!';
       this.errorMessage = '';
+      this.counter = 2;
     });
 
     // ログインに失敗したメッセージを表示する
     this.userService.errorUserLoginSubject.subscribe( () =>{
       this.errorMessage = 'Failed Login...';
       this.successMessage = '';
+      this.counter = 1;
     });
 
     // ユーザ作成に成功したメッセージを表示する
     this.userService.completeRegisterSubject.subscribe( () => {
       this.successMessage = 'Success Signup!';
       this.errorMessage = '';
+      this.counter = 2;
     });
 
     // ユーザ作成に失敗したメッセージを表示する
@@ -46,12 +54,14 @@ export class CommonStore {
       (err) => {
         this.errorMessage = 'Failed Signup...';
         this.successMessage = '';
+        this.counter = 1;
     });
 
     // ログアウトしたメッセージを表示する
     this.userService.logoutUserSubject.subscribe( () => {
       this.successMessage = 'Logged out';
       this.errorMessage = '';
+      this.counter = 2;
     });
 
     // 存在しないユーザIDにアクセスした場合のメッセージを表示する
@@ -59,6 +69,7 @@ export class CommonStore {
       (err) => {
         this.errorMessage = 'There is no account that does not exist';
         this.successMessage = '';
+        this.counter = 1;
     });
 
     // ツイート成功したメッセージを表示する
@@ -66,30 +77,35 @@ export class CommonStore {
       (tweet) => {
         this.successMessage = `Tweeted that "${tweet}".`;
         this.errorMessage = '';
+        this.counter = 1;
     });
 
     // ツイート失敗したメッセージを表示する
     this.tweetService.errorPostTweetSubject.subscribe( () => {
       this.errorMessage = 'Tweet Failed...';
       this.successMessage = '';
+      this.counter = 1;
     });
 
     // ユーザ更新成功したメッセージを表示する
     this.userService.successUpdateUserInfoSubjct.subscribe( () =>{
       this.successMessage = 'Success Update UserInfo';
       this.errorMessage = '';
+      this.counter = 1;
     });
 
     // ユーザ更新失敗したメッセージを表示する
     this.userService.errorUpdateUserInfoSubjct.subscribe( () =>{
       this.errorMessage = 'Failed Update UserInfo...';
       this.successMessage = '';
+      this.counter = 1;
     });
 
     // 新しいパスワードが一致しないメッセージを表示する
     this.userService.notMatchNewPasswordSubject.subscribe( () => {
       this.errorMessage = 'New passwords do not match';
       this.successMessage = '';
+      this.counter = 1;
     });
 
   }
