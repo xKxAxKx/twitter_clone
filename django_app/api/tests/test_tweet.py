89f42900eedf631fc053d4603621cf3c6ed33d79
xKxAxKx <pekepooooong@gmail.com>
2018-03-29 18:16:53 +0900

    ツイートリプライのテスト追加

--- django_app/api/tests/test_tweet.py
+++ django_app/api/tests/test_tweet.py
@@ -63,11 +63,16 @@ class TestTweetGet(TestCaseBase):
         tweet_post_url = os.path.join(self.api_url, 'tweet/post/')
         request_tweet = {'tweet': 'this is test tweet',
                          'parent_tweet': parent_tweet.data}
-        result = self.post(tweet_post_url,
-                           request_tweet,
-                           token)
-        self.assertEqual(status.HTTP_201_CREATED, result.status_code)
-        print(result.data)
+        reply_tweet = self.post(tweet_post_url,
+                                request_tweet,
+                                token)
+        self.assertEqual(status.HTTP_201_CREATED, reply_tweet.status_code)
+        print(reply_tweet.data)
         # ここのレスポンス良くないなー
-        self.assertEqual(result.data['parent'][0]['parent']['tweet'],
+        self.assertEqual(reply_tweet.data['parent'][0]['parent']['tweet'],
                          self.tweet_1_1.tweet)
+
+        re_parent_tweet = self.get(tweet_get_url, None)
+        print(re_parent_tweet.data)
+        self.assertEqual(re_parent_tweet.data['children'][0]['child']['tweet'],
+                         reply_tweet.data['tweet'])
