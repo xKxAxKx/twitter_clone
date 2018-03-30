06413098eb0d19acc3d22fb9ddff9f7bbb70eeae
xKxAxKx <pekepooooong@gmail.com>
2018-03-30 15:09:38 +0900

    tweetのdeleteのtest

--- django_app/api/tests/test_tweet.py
+++ django_app/api/tests/test_tweet.py
@@ -67,12 +67,36 @@ class TestTweetGet(TestCaseBase):
                                 request_tweet,
                                 token)
         self.assertEqual(status.HTTP_201_CREATED, reply_tweet.status_code)
-        print(reply_tweet.data)
+        # print(reply_tweet.data)
         # ここのレスポンス良くないなー
         self.assertEqual(reply_tweet.data['parent'][0]['parent']['tweet'],
                          self.tweet_1_1.tweet)
 
         re_parent_tweet = self.get(tweet_get_url, None)
-        print(re_parent_tweet.data)
+        # print(re_parent_tweet.data)
         self.assertEqual(re_parent_tweet.data['children'][0]['child']['tweet'],
                          reply_tweet.data['tweet'])
+
+    def test_tweet_delete_success(self):
+        token = self.get_user_token()
+        tweet_post_url = os.path.join(self.api_url, 'tweet/post/')
+        request_tweet = {'tweet': 'this is test tweet',
+                         'parent_tweet': None}
+        post_result = self.post(tweet_post_url,
+                                request_tweet,
+                                token)
+        self.assertEqual(status.HTTP_201_CREATED, post_result.status_code)
+
+        tweet_delete_url = os.path.join(self.api_url,
+                                        'tweet/delete/',
+                                        str(post_result.data['id']))
+        del_result = self.delete(tweet_delete_url, None, token)
+        self.assertEqual(status.HTTP_204_NO_CONTENT,
+                         del_result.status_code)
+
+        tweet_get_url = os.path.join(self.api_url,
+                                     'tweet',
+                                     str(post_result.data['id']))
+        get_result = self.get(tweet_get_url, None)
+        self.assertEqual(status.HTTP_404_NOT_FOUND,
+                         get_result.status_code)
