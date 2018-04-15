7b06e7edc8c7c4e74926f2ab43994d7caa70b35f
xKxAxKx <pekepooooong@gmail.com>
2018-04-15 21:27:13 +0900

    update tests

--- django_app/api/tests/test_reply.py
+++ django_app/api/tests/test_reply.py
@@ -1,5 +1,6 @@
 import os
 from rest_framework import status
+from api.models.tweet import Tweet
 from api.tests.test_utils import TestCaseBase
 from api.tests.factory import AccountFactory, TweetFactory
 
@@ -27,6 +28,15 @@ class TestReply(TestCaseBase):
         # 返り値が冗長すぎるのでfixすること!
         self.assertEqual(self.tweet_1_1.id,
                          result.data['parent'][0]['parent']['id'])
-
         # 理想の返り返り値は以下になると思う
         # self.assertEqual(self.tweet_1_1.id, result.data['parent']['id'])
+
+        tweet_get_url = os.path.join(self.api_url,
+                                     'tweet',
+                                     str(self.tweet_1_1.id))
+        parent_result = self.get(tweet_get_url, None)
+        self.assertEqual(result.data['id'],
+                         parent_result.data['children'][0]['child']['id'])
+        # ここもこうしたい
+        # self.assertEqual(result.data['id'],
+        #                  parent_result.data['children'][0]['id'])
