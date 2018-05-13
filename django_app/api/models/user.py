94a6ce246a5b8eb6e80ef4ace65c427984bf7f6f
xKxAxKx <pekepooooong@gmail.com>
2018-05-13 17:01:42 +0900

    flake8

--- django_app/api/models/user.py
+++ django_app/api/models/user.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, _user_has_perm
 )
-from django.core import validators
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
