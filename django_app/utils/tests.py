be1d2b7cf2ac3ecc489c5cb5e37dee14b5ea858c
xKxAxKx <pekepooooong@gmail.com>
2018-06-03 21:43:32 +0900

    update:test

--- django_app/utils/tests.py
+++ django_app/utils/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from user.models import User
 
-# Create your tests here.
+class TwitterTestCase(TestCase):
+
+    def setUp(self):
+        for i in range(0, 3):
+            email = 'test_{}@test.com'.format(str(i))
+            username = 'test_{}'.format(str(i))
+            profile = 'test_{}'.format(str(i))
+            User.objects.create(email=email,
+                                username=username,
+                                profile=profile)
+
+        self.users = User.objects.all()
