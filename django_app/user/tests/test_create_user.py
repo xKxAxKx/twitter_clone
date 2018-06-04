e3c02514b7fc95b5f62920883f8093b1fcae5ba7
xKxAxKx <pekepooooong@gmail.com>
2018-06-04 22:54:51 +0900

    test...

--- django_app/user/tests/test_create_user.py
+++ django_app/user/tests/test_create_user.py
@@ -4,5 +4,10 @@ from utils.tests import TwitterTestCase
 class TestCreateUser(TwitterTestCase):
 
     def test_create_user(self):
+
+        data = {"email": "test_user@example.com",
+                "username": "test_user",
+                "password": "password"}
+
         import pdb;
         pdb.set_trace()
--- django_app/user/urls.py
+++ django_app/user/urls.py
@@ -4,8 +4,8 @@ from rest_framework_jwt.views import (obtain_jwt_token, refresh_jwt_token,
 from user.views import CreateUser
 
 urlpatterns = [
-    url(r'^login/$', obtain_jwt_token),
-    url(r'^token_refresh/$', refresh_jwt_token),
-    url(r'^token_verify/$', verify_jwt_token),
-    url(r'^create_user/$', CreateUser.as_view()),
+    url(r'^login$', obtain_jwt_token),
+    url(r'^token_refresh$', refresh_jwt_token),
+    url(r'^token_verify$', verify_jwt_token),
+    url(r'^create_user$', CreateUser.as_view()),
 ]
