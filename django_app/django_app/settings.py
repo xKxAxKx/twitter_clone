72033df3644d6fd9d08749502b0d9941bf9b77af
xKxAxKx <pekepooooong@gmail.com>
2018-07-14 16:29:08 +0900

    update:settings

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -139,10 +139,12 @@ JWT_AUTH = {
 }
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
