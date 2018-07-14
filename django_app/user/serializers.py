53b753e6697970a17dc51219d90d29acf2beb4c2
xKxAxKx <pekepooooong@gmail.com>
2018-07-14 16:29:58 +0900

    fix:serializerにcreateメソッドを追加して、正しくユーザ作成されるようにした

--- django_app/user/serializers.py
+++ django_app/user/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from user.models import User
 
+
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'profile', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(request_data=validated_data)
