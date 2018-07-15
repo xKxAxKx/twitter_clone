6e7930cc3c11bf98ac8a9e412b2bbdf47acc914b
xKxAxKx <pekepooooong@gmail.com>
2018-07-15 19:31:44 +0900

    update:ユーザ作成周りのテスト追加

--- django_app/user/tests/test_create_user.py
+++ django_app/user/tests/test_create_user.py
@@ -27,3 +27,31 @@ class TestCreateUser(TwitterTestCase):
                                   data,
                                   format='json')
         self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_fail_create_user_lack_username(self):
+        data = {"email": "test_1example.com",
+                "password": "password"}
+
+        result = self.client.post('/api/user/register',
+                                  data,
+                                  format='json')
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_fail_create_user_lack_password(self):
+        data = {"email": "test_1example.com",
+                "username": "test_1"}
+
+        result = self.client.post('/api/user/register',
+                                  data,
+                                  format='json')
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_fail_create_user_password_empty(self):
+        data = {"email": "test_1example.com",
+                "username": "test_1",
+                "password": ""}
+
+        result = self.client.post('/api/user/register',
+                                  data,
+                                  format='json')
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
