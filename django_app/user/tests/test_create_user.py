e0991e5409d8038995dedd70b3d3f4adb86f1a0c
xKxAxKx <pekepooooong@gmail.com>
2018-07-15 19:27:09 +0900

    update:ユーザ作成テスト：正しくないemail

--- django_app/user/tests/test_create_user.py
+++ django_app/user/tests/test_create_user.py
@@ -17,3 +17,13 @@ class TestCreateUser(TwitterTestCase):
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         self.assertEqual(result.data['email'], 'test_1@example.com')
         self.assertEqual(result.data['username'], 'test_1')
+
+    def test_fail_create_user_invalid_email(self):
+        data = {"email": "test_1example.com",
+                "username": "test_1",
+                "password": "password"}
+
+        result = self.client.post('/api/user/register',
+                                  data,
+                                  format='json')
+        self.assertEqual(result.status_code, status.HTTP_400_BAD_REQUEST)
